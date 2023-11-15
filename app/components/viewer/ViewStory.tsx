b814b0d110e9cbdb1044822bde98c9bc6707356f
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-11-15 08:36:33 -0500

    nice

--- app/components/viewer/ViewStory.tsx
+++ app/components/viewer/ViewStory.tsx
@@ -93,6 +93,7 @@ export default function ViewStory({ fullData }: { fullData: StoryData }) {
     const [userTriedToDelete, userTriedToDeleteSet] = useState(false)
     const [wantsToEditCurrentGamemodes, wantsToEditCurrentGamemodesSet] = useState(false)
     const [canPlayAudio, canPlayAudioSet] = useState(false)
+    const [canShowAudioControls, canShowAudioControlsSet] = useState(false)
 
     const [typedPass, typedPassSet] = useState("")
     const [errorsSeen, errorsSeenSet] = useState<{
@@ -214,16 +215,21 @@ export default function ViewStory({ fullData }: { fullData: StoryData }) {
                 <div className={`${styles.readingArea} niceScrollbar`} style={{ gridTemplateColumns: gameModesShowing ? "1fr 1fr" : "1fr" }}>
                     <div>
                         <div style={{ position: "absolute", right: 0, margin: "1rem", display: "flex", alignItems: "center", gap: "1rem" }}>
-                            <ReactPlayer
-                                loop={true}
-                                playing={canPlayAudio}
-                                url={fullData.backgroundaudio ? fullData.backgroundaudio : "https://www.youtube.com/watch?v=NJuSStkIZBg"}
-                                height={50}
-                                width={50}
-                                onPlay={() => canPlayAudioSet(true)}
-                            />
+                            <div style={{ display: canShowAudioControls ? "none" : "block" }}>
+                                <ReactPlayer
+                                    loop={true}
+                                    playing={canPlayAudio}
+                                    url={fullData.backgroundaudio ? fullData.backgroundaudio : "https://www.youtube.com/watch?v=NJuSStkIZBg"}
+                                    height={50}
+                                    width={50}
+                                    onPlay={() => {
+                                        canPlayAudioSet(true)
+                                        canShowAudioControlsSet(true)
+                                    }}
+                                />
+                            </div>
 
-                            <div style={{ width: "1rem", aspectRatio: "1/1", fill: "var(--textColor)" }} onClick={() => { canPlayAudioSet(prev => !prev) }}>
+                            {canShowAudioControls && <div style={{ width: "1rem", aspectRatio: "1/1", fill: "var(--textColor)" }} onClick={() => { canPlayAudioSet(prev => !prev) }}>
                                 {canPlayAudio ? (
                                     <>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z" /></svg>
@@ -233,7 +239,7 @@ export default function ViewStory({ fullData }: { fullData: StoryData }) {
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" /></svg>
                                     </>)
                                 }
-                            </div>
+                            </div>}
                         </div>
 
                         <span style={{ display: "flex", gap: ".5rem", alignItems: "center", padding: ".5rem" }}>
